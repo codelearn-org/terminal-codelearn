c641477d78fab25d714459666c7ea3f7c74204b6
pocha <pocha@stalkninja.com>
2013-07-26 17:23:40 +0530

    fixed user memory limits

--- create-jail-and-chroot.sh
+++ create-jail-and-chroot.sh
@@ -13,7 +13,8 @@ mkdir $NEW_JAIL_DIR
 #cp -r $JAIL_DIR/etc $NEW_JAIL_DIR/etc
 #echo "root:x:0:0:root:/home:/bin/bash
 mkdir $NEW_JAIL_DIR/etc
-echo "$USER:x:1002:1002::/home:/bin/bash" > $NEW_JAIL_DIR/etc/passwd
+echo "$USER:x:1001:1001::/home:/bin/bash" > $NEW_JAIL_DIR/etc/passwd
+cp $JAIL_DIR/etc/group $NEW_JAIL_DIR/etc/group
 
 #mkdir $NEW_JAIL_DIR/usr
 #mkdir $NEW_JAIL_DIR/lib
@@ -29,5 +30,5 @@ echo "HOME=/home; cd /home; alias ls='ls --color=auto'; export PS1='\[\e[35m\]\w
 
 #chroot --userspec guest $NEW_JAIL_DIR /bin/bash --rcfile /home/.bashrc
 #chroot --userspec $USER $NEW_JAIL_DIR /bin/bash --rcfile /home/.bashrc -c "su user_16"
-su $USER -c "ulimit -d 20000 -v 20000 -f 10000 -n 100 -s 512 -t 60 -x 100 -l 64;
+su $USER -c "ulimit -d 20000 -v 20000 -f 1000 -n 100 -s 512 -t 20 -x 20 -l 64;
 proot -b /bin:/bin -b /usr:/usr -b /lib:/lib -b /dev:/dev -r $DIR -w /home  /bin/bash --rcfile /home/.bashrc"
